86e858ea9fe08db0b99d2b889a45a6d93ea58e0e
Oleguzik <guzik.oleg@gmail.com>
2022-09-01 00:33:59 +0200

    Third Task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const formRef = document.querySelector('form.form');
+formRef.addEventListener('submit', onPromiseFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.3) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onPromiseFormSubmit(e) {
+  e.preventDefault();
+  const { amount, delay, step } = e.currentTarget;
+  const numOfPromises = amount.value;
+  const initialDelay = delay.value;
+  const additionalDelay = step.value;
+  console.log(numOfPromises);
+
+  for (let i = 0; i < numOfPromises; i += 1) {
+    const promiseDelay = initialDelay * 1 + i * additionalDelay;
+    console.log(`Creating promise #${i + 1} with delay ${promiseDelay}`);
+    createPromise(i + 1, promiseDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
